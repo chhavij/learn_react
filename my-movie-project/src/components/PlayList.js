d94adf68da8b3fd6ae0ee677ffcc098f4c416f8e
Chhavi Jain <chhavijain.13@gmail.com>
2021-05-12 14:09:59 -0700

    Displaying hardcoded data using PlayList component.

--- my-movie-project/src/components/PlayList.js
+++ my-movie-project/src/components/PlayList.js
@@ -1,1 +1,24 @@
 import React from 'react'
+/*
+Functional componnet to display list of movies. 
+*/
+
+function PlayList(props) {
+    return (
+        <>
+            {props.movies.map((movie) => (
+                <div key={movie.imdbID} className="card">
+                    <img src={movie.Poster} className="card-img-top h-100"/>
+                    <div className="card-body">
+                    <h5 className="card-title">{movie.Title}</h5>
+                     <p className="card-subtitle">{movie.Year}</p>
+                    </div>
+                </div>  
+            ))}
+        </>
+
+    )
+}
+
+export default PlayList
+
